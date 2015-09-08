bc457de92ee8ff34a1f13c77648c029b721de32f
TangJaven <15907097049@163.com>
2015-09-08 11:07:43 +0800

    Create HelloJavenActivity.java

---
+++ HelloJavenActivity.java
@@ -0,0 +1,11 @@
+import android.app.Activity
+
+
+public class HelloJavenActivity extends Activity {
+  
+  @Override
+  protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+  }
+  
+}
